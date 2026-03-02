5c33e71de4c67ab5845cd1b4de108878d0f38edd
Jesse <jessef15@gmail.com>
2026-03-02 17:53:15 -0500

    fix path aliases

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Allow the scraper to run as a cron job via API route
-  experimental: {
-    serverActions: { allowedOrigins: ["localhost:3000"] },
-  },
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
